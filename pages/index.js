0fc595580433280833574bad7b472a653e20b602
wuzwutz <74846559+wuzwutz@users.noreply.github.com>
2022-04-06 00:43:56 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Weme to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         test
         </p>
       </main>
 
